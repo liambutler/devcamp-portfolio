3b4f490170a27eb03680e72bc116d0339ca524dc
Liam Butler <lbutler@zendesk.com>
2017-05-26 10:07:54 +0200

    Building out portfolio page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do | blog |
+  Blog.create!(
+          title: "My blog post # #{blog}",
+          body: "Just some words"
+  )
+end
+
+5.times do | portfolio |
+  Portfolio.create!(
+      title: "Portfolio item # #{portfolio}",
+      subtitle: "Isn't this great?",
+      body: "Lorem ipsum dolor sit amet, id dolorum denique philosophia sit, recteque euripidis scripserit no cum. Nec cu populo blandit, everti quaestio pro an. No vim eius lobortis. Idque facete incorrupte vis ne, ad mei graeco tacimates eloquentiam.
+              Ea mundi apeirian consequat his, fabulas vivendum moderatius nam ex. Mei tota ullum dolor ad, pro probo everti ut, mutat etiam elitr et vix. Pro virtute facilisis laboramus ex, eu mutat eloquentiam sed, mea labitur aliquid te. Tantas equidem ei mea. Vide graeci suscipiantur ut eam, ius choro prodesset et, justo regione no vis.",
+      main_image: 'http://placehold.it/600x350',
+      thumb_image: 'http://placehold.it/350x150'
+  )
+end
